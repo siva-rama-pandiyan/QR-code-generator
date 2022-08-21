a85a14a6c780ef3200514a579f3839e52b999b01
siva-rama-pandiyan <sivaamc15@gmail.com>
2022-08-21 19:47:54 +0530

    Javascript functionalities added to generate qr code

--- js/script.js
+++ js/script.js
@@ -4,10 +4,60 @@ const qr = document.getElementById("qrcode");
 const onGenerateSubmit = (e) => {
   e.preventDefault();
 
+  clearUi();
+
   const url = document.getElementById("url").value;
   const size = document.getElementById("size").value;
 
-  console.log(url, size);
+  if (url === "") {
+    alert("Please enter a URL ");
+  } else {
+    showSpinner();
+    setTimeout(() => {
+      hideSpinner();
+      generateQrCode(url, size);
+      setTimeout(()=>{
+        const saveUrl = qr.querySelector('img').src;
+
+        createSaveButton(saveUrl);
+      },50);
+    }, 1000);
+  }
 };
 
+const generateQrCode = (url, size) =>{
+ const qrCode = new QRCode('qrcode', {
+  text: url,
+  width: size,
+  height: size
+ })
+};
+
+const showSpinner = () => {
+  document.getElementById("spinner").style.display = "block";
+};
+
+const hideSpinner = () => {
+  document.getElementById("spinner").style.display = "none";
+};
+
+const clearUi = () =>{
+  qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link')
+  if(saveLink){
+    saveLink.remove();
+  }
+};
+
+const createSaveButton = (saveUrl) =>{
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList = 'bg-red-500 hover:bg-red-700 tex-white font-bold py-2 rounded w-1/3 m-auto my-5'
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = 'Save Image';
+  document.getElementById('generated').appendChild(link)
+};
+
+hideSpinner();
 form.addEventListener("submit", onGenerateSubmit);
